614c524ffac7d022cac0b5504989be132d699d7c
Brandon <martinezbran0221@gmail.com>
2025-11-24 16:51:37 -0500

    Disable service worker and simplify store exports

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,12 @@
+// Re-export hooks
 export { useAppDispatch, useAppSelector } from './hooks'
 export { useSavedProducts } from './hooks/useSavedProducts'
 export { useAuthRedux } from './hooks/useAuthRedux'
 
-export { setCurrentPage } from './slices/appSlice'
-export { setAuth, setLoading, clearAuth } from './slices/authSlice'
-export {
-  setSavedProducts,
-  addSavedProduct,
-  removeSavedProduct,
-  clearSaved,
-} from './slices/savedSlice'
-
+// Re-export types
 export type { RootState, AppDispatch } from './store'
 export type { SavedProduct } from './slices/savedSlice'
+
+// Re-export actions if needed by components
+export { setCurrentPage } from './slices/appSlice'
+export { setAuth, setLoading, clearAuth } from './slices/authSlice'
